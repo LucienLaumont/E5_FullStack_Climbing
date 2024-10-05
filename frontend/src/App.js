9a6a2db08df352d5994553de3c85aa5e43aa5738
lucienlaumont <lucienlaumont36@gmail.com>
2024-10-05 12:19:13 +0200

    UpdateFrontPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import './App.css';
+import climbingImage from './assets/frontpage.png'; // Assure-toi que l'image soit dans le bon dossier
 
 function App() {
-  const [climbers, setClimbers] = useState([]);
-
-  // Effectuer un appel à l'API pour récupérer la liste des grimpeurs
-  useEffect(() => {
-    fetch('http://localhost:5000/climbers/?skip=0&limit=10') // Assurez-vous que cette route existe dans votre API FastAPI
-      .then(response => response.json())
-      .then(data => setClimbers(data))
-      .catch(error => console.error('Error fetching climbers:', error));
-  }, []);
-
   return (
     <div className="App">
-      <h1>Climbing Profiles</h1>
-      <ul>
-        {climbers.map(climber => (
-          <li key={climber.user_id}>
-            <strong>Country:</strong> {climber.country} <br />
-            <strong>Sex:</strong> {climber.sex === 1 ? "Male" : "Female"} <br />
-            <strong>Height:</strong> {climber.height} cm <br />
-            <strong>Weight:</strong> {climber.weight} kg <br />
-            <strong>Age:</strong> {climber.age} years <br />
-            <strong>Years Climbing:</strong> {climber.years_cl} <br />
-            <strong>First Climb Date:</strong> {new Date(climber.date_first).toLocaleDateString()} <br />
-            <strong>Last Climb Date:</strong> {new Date(climber.date_last).toLocaleDateString()} <br />
-            <strong>Grades Count:</strong> {climber.grades_count} <br />
-            <strong>Grades First:</strong> {climber.grades_first} <br />
-            <strong>Grades Last:</strong> {climber.grades_last} <br />
-            <strong>Grades Max:</strong> {climber.grades_max} <br />
-            <strong>Grades Mean:</strong> {climber.grades_mean.toFixed(2)} <br />
-            <strong>First Climb Year:</strong> {climber.year_first} <br />
-            <strong>Last Climb Year:</strong> {climber.year_last} <br />
-          </li>
-        ))}
-      </ul>
+      {/* Image de fond */}
+      <img className="full-screen-image" src={climbingImage} alt="Climbing App Frontpage" />
+
+      {/* Boutons superposés */}
+      <div className="overlay">
+        <a href="https://example.com/climbers" className="clickable-block">
+          <h2>Dashboard</h2>
+        </a>
+        <a href="http://localhost:5000/docs" className="clickable-block">
+          <h2>API</h2>
+        </a>
+        <a href="https://github.com/LucienLaumont" className="clickable-block">
+          <h2>GitHub</h2>
+        </a>
+      </div>
     </div>
   );
 }
